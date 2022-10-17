74b2dc53fd9e97db564ce10405c5bce53b573694
zhuge_ning17@hotmail.com <zhuge_ning17@hotmail.com>
2022-10-17 10:58:26 +0700

    get json data prop

--- components/table.js
+++ components/table.js
@@ -1,4 +1,5 @@
 import {BiEdit, BiTrashAlt} from "react-icons/bi"
+import data from '../database/data.json'
 
 export default function Table(){
     return (
@@ -6,7 +7,7 @@ export default function Table(){
             <thead>
                 <tr className="bg-gray-800">
                     <th className="px-16 py-2">
-                        <span className="text-gray-200">Transformer Topic</span>
+                        <span className="text-gray-200">Device Number</span>
                     </th>
                     <th className="px-16 py-2">
                         <span className="text-gray-200">Text</span>
@@ -26,29 +27,37 @@ export default function Table(){
                 </tr>
             </thead>
             <tbody className="bg-gray-200">
-                <tr className="bg-gray-50 text-center">
+                {
+                    data.map((obj,i)=> <Tr{...obj}key={i}/>)
+                }
+            </tbody>
+        </table>
+    )
+}
+
+function Tr({name,text,date,responsibility,status}){
+    return(
+        <tr className="bg-gray-50 text-center">
                     <td className="px-16 py-2 flex flex-row items-center">
                         <img src="" alt="" />
-                        <span className="text-center ml-2 font-semibold">Transformer Failure</span>
+                        <span className="text-center ml-2 font-semibold">{name}</span>
                     </td>
                     <td className="px-16 py-2">
-                        <span>Bushing transformer oil leakage phase A</span>
+                        <span>{text}</span>
                     </td>
                     <td className="px-16 py-2">
-                        <span>10.10.2022</span>
+                        <span>{date}</span>
                     </td>
                     <td className="px-16 py-2">
-                        <span>Wutthipan</span>
+                        <span>{responsibility}</span>
                     </td>
                     <td className="px-16 py-2">
-                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">waiting</span></button>
+                        <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status}</span></button>
                     </td>
                     <td className="px-16 py-2 flex justify-around gap-5">
                         <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94"}></BiEdit></button>
                         <button className="cursor"><BiTrashAlt size={25} color={"rgb(244,63,94"}></BiTrashAlt></button>
                     </td>
                 </tr>
-            </tbody>
-        </table>
     )
 }
